60e95796a05562e20eaaa0972423c8da6483a726
igami <igami@users.noreply.github.com>
2021-02-10 08:32:54 +0100

    install vscode via ansible-galaxy

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-sudo apt -y install ansible
+# install requirements
+sudo apt -y install ansible git
+
+# clone repository
+rm -rf /tmp/deployment
+git clone https://github.com/Igami/deployment.git /tmp/deployment
+cd /tmp/deployment
+
+# deploy vi ansbile
+# https://galaxy.ansible.com/docs/using/installing.html
+ansible-galaxy install -r requirements.yml
+ansible-playbook deployment.yml
